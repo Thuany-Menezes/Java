c80726b2c28c11629b4554f0d535ce2267d797c8
Thuany-Menezes <106390561+Thuany-Menezes@users.noreply.github.com>
2023-09-08 17:13:33 -0300

    Update import java.util.java

--- Calculadora/import java.util.java
+++ Calculadora/import java.util.java
@@ -1,16 +1,23 @@
-import java.util.Locale;
+package calculadora;
+
 import java.util.Scanner;
 
-public class Main {
-public static void main(String[] args) {
-Scanner numero1Scanner = new Scanner(System.in);
-System.out.print("Digite o primeiro número: ");
-double numero1 = numero1Scanner.nextDouble();
-Scanner numero2Scanner = new Scanner(System.in);
-System.out.print("Digite o segundo número: ");
-double numero2 = numero2Scanner.nextDouble();
-double total=numero1+numero2;
-System.out.print("O resultado eh: "+Math.round(total));
+public class Main{
 
-}
+    public static void main(String[] args){
+        int n1;
+        int n2;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Insira o primeiro numero:");
+        n1 = input.nextInt();
+        System.out.println("Insira o segundo numero:");
+        n2 = input.nextInt();
+
+        System.out.println("Soma:"+(n1+n2));
+        System.out.println("Subtração:"+(n1-n2));
+        System.out.println("Divisão:"+(n1/n2));
+        System.out.println("Multiplicação:"+(n1*n2));
+    }
 }
